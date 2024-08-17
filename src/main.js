589af1175ce0c7f2ab7fb6f73b1c206832e166ea
Jeremy <jeremy.laumon@gmail.com>
2024-08-17 22:08:48 +0200

    Added very basic map with castle and trees

--- src/main.js
+++ src/main.js
@@ -18,19 +18,13 @@ scene.camera.to.set(0);
 let tileAtlasImage = new Image();
 tileAtlasImage.src="src/atlas.png";
 
-class Vec2 {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
+class Vec2 extends Point {
   add(vec) {
     return new Vec2(vec.x + this.x, vec.y + this.y);
   }
 }
 
 const TILE_SIZE = 8;
-const MAP_SIZE  = new Vec2(31, 23);
 const MAP_POS   = new Vec2(8, 8);
 
 const gameStart = () => {
@@ -51,26 +45,101 @@ const gameStart = () => {
   const TILE_GRASS_1      = new TileResource(1, 1);
   const TILE_GRASS_2      = new TileResource(1, 2);
   const TILE_GRASS_3      = new TileResource(1, 3);
-  const TILE_TREE_BOTTOM  = new TileResource(2, 0);
+  const TILE_TREE_TOP     = new TileResource(2, 0);
   const TILE_TREE_MIDDLE  = new TileResource(2, 1);
-  const TILE_TREE_TOP     = new TileResource(2, 2);
+  const TILE_TREE_BOTTOM  = new TileResource(2, 2);
   const TILE_TREE_SMALL   = new TileResource(2, 3);
+  const TILE_CASTLE_TL    = new TileResource(3, 0);
+  const TILE_CASTLE_TR    = new TileResource(4, 0);
+  const TILE_CASTLE_BL    = new TileResource(3, 1);
+  const TILE_CASTLE_BR    = new TileResource(4, 1);
 
 
-  const layer = scene.layer(0);
+  const InitialMap = [
+    "TTTTTTt.......................",
+    "TTTTTt........................",
+    "TTTt..........................",
+    "TTt...........................",
+    "t.............................",
+    "..............................",
+    "..............................",
+    "..............................",
+    "..............................",
+    "..............cc..............",
+    "..............cc..............",
+    "..............................",
+    "..............................",
+    "..............................",
+    "..............................",
+    "..............................",
+    "..............................",
+    "...T..........................",
+    "..TTt.........................",
+    ".TTTTT........................",
+  ];
+
+  const MAP_SIZE  = new Vec2(InitialMap[0].length, InitialMap.length);
+
+  const addTile = (layer, tile, x, y) => {
+    const sprite = new Sprite(tile.frame);
+    const tile_pos = new Vec2(x * TILE_SIZE + MAP_POS.x, y * TILE_SIZE + MAP_POS.y);
+
+    sprite.position.set(tile_pos.x , tile_pos.y);
+
+    layer.add(sprite);
+  }
+
+  const map_layer_base = scene.layer(0);
+  const map_layer_top = scene.layer(1);
 
   const mud_tiles = [TILE_MUD_0, TILE_MUD_1, TILE_MUD_2];
   
   for (let y = 0; y < MAP_SIZE.y; y++) {
     for (let x = 0; x < MAP_SIZE.x; x++) {
 
-      const sprite = new Sprite(mud_tiles[Math.floor(Math.random() * mud_tiles.length)].frame);
-      const tile_pos = new Vec2(x * TILE_SIZE + MAP_POS.x, y * TILE_SIZE + MAP_POS.y);
+      const c = InitialMap[y][x];
 
-      sprite.position.set(tile_pos.x , tile_pos.y);
-      // sprite.scale.set(4, 4);
+      let tile;
 
-      layer.add(sprite);
+      if (c == '.')
+      {
+        tile = mud_tiles[Math.floor(Math.random() * mud_tiles.length)];
+      }
+      else if (c == 't')
+      {
+        tile = TILE_TREE_SMALL;
+      }
+      else if (c == 'T')
+      {
+        // If there's another tree below (or it's the bottom of the map), then it's a middle 
+        if (y == MAP_SIZE.y - 1 || InitialMap[y + 1][x] == 'T')
+        {
+          tile = TILE_TREE_MIDDLE;
+
+          // If there isn't another tree above, add a tree top there.
+          if (y > 0 && InitialMap[y - 1][x] != 'T')
+          {
+            addTile(map_layer_top, TILE_TREE_TOP, x, y - 1);
+          }
+        }
+        else
+        {
+          tile = TILE_TREE_BOTTOM;
+        }
+      }
+      else if (c == 'c')
+      {
+        const isTop  = y < (MAP_SIZE.y - 1) && InitialMap[y + 1][x] == 'c';
+        const isLeft = x < (MAP_SIZE.x - 1) && InitialMap[y][x + 1] == 'c';
+
+        if (isTop)
+          tile = isLeft ? TILE_CASTLE_TL : TILE_CASTLE_TR;
+        else
+          tile = isLeft ? TILE_CASTLE_BL : TILE_CASTLE_BR;
+      }
+
+      if (tile)
+        addTile(map_layer_base, tile, x, y);
     }
   }
 
